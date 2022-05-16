b2844175d9e237cc3274654ec52e4834a6b8ec12
Helen <grits.lena@gmail.com>
2022-05-16 22:42:09 +0300

    minor changes

--- js/domManipulators/lotsPrinter.js
+++ js/domManipulators/lotsPrinter.js
@@ -8,20 +8,19 @@ const q = query(lotsRef);
 const querySnapshot = await getDocs(q);
 
 const itemsRef = collection(myDB.db, "items");
-// var name = "";
-// var description = "";
 
 
-function func(doc, callback) {
-    const smth = [2];
+
+function getItemNameAndDescription(doc, callback) {
+    const itemInfo = [2];
     const q = query(itemsRef, where("id_", "==", doc.data().itemId));
     getDocs(q).then(querySnapshot2=>{
         querySnapshot2.forEach((doc_) => {
-            smth[0] = doc_.data().itemName;
-            smth[1] = doc_.data().description;
+            itemInfo[0] = doc_.data().itemName;
+            itemInfo[1] = doc_.data().description;
         });
-        console.log(smth);
-        callback(smth);
+        console.log(itemInfo);
+        callback(itemInfo);
     });
 }
 
@@ -29,13 +28,13 @@ querySnapshot.forEach((doc) => {
     const startingPrice = doc.data().startingPrice;
     const step = doc.data().auctionStep;
     const date = doc.data().startDate.replace("T", " ");
-    const id__ =  doc.data().itemId;
-    func(doc, (ret)=>{
-        console.log(ret[0], ret[1]);
+    //const id__ =  doc.data().itemId;
+    getItemNameAndDescription(doc, (returnedInfo)=>{
+        console.log(returnedInfo[0], returnedInfo[1]);
         const inDiv = document.createElement('div');
         inDiv.innerHTML = "<div class=\"lot-preview\">" +
-            "            <p class=\"item-info\">Name: \"" + ret[0] + "\"</p>" +
-            "            <p class=\"item-info\">Description: \"" + ret[1] + "\"</p>" +
+            "            <p class=\"item-info\">Name: \"" + returnedInfo[0] + "\"</p>" +
+            "            <p class=\"item-info\">Description: \"" + returnedInfo[1] + "\"</p>" +
             "            <p class=\"item-info\">Starting price: \"" + startingPrice + "\"</p>" +
             "            <p class=\"item-info\">Auction step: \"" + step + "\"</p>" +
             "            <hr style=\"width: 95%;\">" +
