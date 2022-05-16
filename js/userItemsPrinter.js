3ce5f74a2237e61c670bb25b5fba58830c9ed7db
Helen <grits.lena@gmail.com>
2022-05-16 18:26:42 +0300

    add items printer

---
+++ js/userItemsPrinter.js
@@ -0,0 +1,22 @@
+import {myDB} from "./db/db.js";
+import{myItemsScroll} from "./general.js";
+
+import {collection, where, query, getDocs} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+
+const itemsRef = collection(myDB.db, "items");
+const q = query(itemsRef, where("ownerId", "==", localStorage.getItem("name")));
+const querySnapshot = await getDocs(q);
+querySnapshot.forEach((doc)=>{
+    const name = doc.data().itemName;
+    const description = doc.data().description;
+    const inDiv = document.createElement('div');
+    inDiv.innerHTML = "        <div class=\"lot-preview\">" +
+        "            <label class=\"item-info\">Name: \""+name+"\"</label><br>" +
+        "            <label class=\"item-info\">Description: \""+description+"\" </label><br>" +
+        "            <hr style=\"width: 95%\">" +
+        "            <button class=\"delete\">" +
+        "                <i class=\"button-text\">delete</i>" +
+        "            </button>" +
+        "        </div>";
+    myItemsScroll.appendChild(inDiv);
+});
