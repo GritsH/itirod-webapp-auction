63d43ef7a1fce3e8dcf58e48033364ce34810737
Helen <grits.lena@gmail.com>
2022-05-16 22:50:13 +0300

    minor changes

--- js/db/db.js
+++ js/db/db.js
@@ -48,7 +48,7 @@ class MyDB {
     async addItem(item) {
         try {
             const docRef = await setDoc(doc(this.db, "items", item.id.toString()), {
-                id_: item.id.toString(),
+                id: item.id,
                 description: item.description,
                 itemName: item.itemName,
                 itemType: item.itemType,
